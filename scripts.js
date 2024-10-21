c6a46b1abee618a0e65b5dfc3edb307923a934ba
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-10-21 11:03:37 -0700

    Update text styles to appear/dissapear

--- scripts.js
+++ scripts.js
@@ -7,25 +7,31 @@ rec.lang = "en-US";
 rec.continuous = false;
 
 const startButton = document.querySelector(".start-button");
-const stopButton = document.querySelector(".stop-button");
+const resetButton = document.querySelector(".reset-button");
 
 const textAppear = document.querySelector(".after-start");
 const textDissapear = document.querySelector(".before-start");
+const infoText = document.querySelector(".info-text");
 
 const speechText = document.querySelector(".speech-text");
 const output = document.querySelector(".speech-output");
 
 startButton.addEventListener("click", () => {
   output.style.display = "hidden";
+  infoText.style.display = "none";
   textDissapear.style.display = "none";
   textAppear.style.display = "block";
   rec.start();
 });
 
-stopButton.addEventListener("click", () => {
+resetButton.addEventListener("click", () => {
+  infoText.style.display = "block";
   textDissapear.style.display = "block";
   textAppear.style.display = "none";
   rec.stop();
+  output.innerText = "";
+  output.classList.remove("alert", "alert-primary", "mt-3");
+  speechText.innerText = "";
 });
 
 rec.onresult = function (e) {
